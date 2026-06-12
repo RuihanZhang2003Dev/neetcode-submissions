72267bffe3b0b67a06ec411cc43bbd8964d7eb30
RZ0623 <rainzhang3166@gmail.com>
2026-06-12 12:23:19 -0400

    Add: two-integer-sum - submission-14

---
+++ Data Structures & Algorithms/two-integer-sum/submission-14.ts
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums: number[], target: number): number[] {
+        const set = new Set<[number, number]>();
+        for (let i = 0; i< nums.length; i++){
+            for (let j = i+1; j< nums.length; j++){
+                if (set.has([nums[i], nums[j]]) || set.has([nums[j], nums[i]])){
+                    continue;
+                }
+                if (nums[i] + nums[j] == target){
+                    return [i, j];
+                }
+                set.add([i, j]);
+            }
+        }
+    }
+}
