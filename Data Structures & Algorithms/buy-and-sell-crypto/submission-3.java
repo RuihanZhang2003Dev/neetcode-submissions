d681d3ed785d843ef96100016313844454e46f2b
RZ0623 <rainzhang3166@gmail.com>
2026-09-19 20:16:12 -0400

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        /*
+        idea: iterate through the prices and compare, keep the minimum of the past
+        so that we can compare profit with today's price
+        */
+        int maxProfit = 0;
+        int curMin= prices[0];
+        for (int i = 1; i< prices.length; i++){
+            curMin = Math.min(prices[i], curMin);
+            if (prices[i] > curMin) maxProfit = Math.max(maxProfit, prices[i] - curMin);
+        }
+
+        return maxProfit;
+    }
+}
