851b01b625d87b5707269a5697df31578f092cb0
RZ0623 <rainzhang3166@gmail.com>
2026-09-26 15:04:19 -0400

    Add: longest-common-prefix - submission-9

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-9.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        /*
+        idea: sort the string, then compare the first and last word, to find the
+        longest common prefix
+        */
+        Arrays.sort(strs);
+        StringBuilder result = new StringBuilder();
+        for (int i = 0 ; i< strs[0].length() && i < strs[strs.length -1].length(); i++){
+            if (strs[0].charAt(i) == strs[strs.length -1].charAt(i)) result.append(strs[0].charAt(i));
+            else break;
+        }
+        return result.toString();
+    }
+}
